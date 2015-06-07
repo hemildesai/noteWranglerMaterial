210ef1e704f5cfee04a55be22f64cdfb95048631
hemildesai <desai38@purdue.edu>
2015-06-07 15:13:02 +0530

    created and use ngGravatar module

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -1,9 +1,12 @@
 (function () {
-  angular.module('NoteWrangler', ['ngMaterial', 'ngRoute', 'ngResource'])
+  angular.module('NoteWrangler', ['ngMaterial', 'ngRoute', 'ngResource', 'ngGravatar'])
   .config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
     .primaryPalette('teal')
     .accentPalette('light-green');
+  })
+  .config(function(GravatarProvider){
+    GravatarProvider.setSize(400);
   })
   .controller('MainController', function($scope, $timeout, $mdSidenav, $mdUtil, $log) {
     $scope.toggleSideNav = buildToggler();
